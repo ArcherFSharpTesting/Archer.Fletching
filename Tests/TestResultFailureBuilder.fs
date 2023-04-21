b46b88e140e9247e4bfac8cd9879b6b6e9574053
Jason Kerney <robert.kerney@edf-re.com>
2023-04-21 13:32:34 -0700

    test: TestResultFailureBuilder

--- Tests/TestResultFailureBuilder.fs
+++ Tests/TestResultFailureBuilder.fs
@@ -21,11 +21,148 @@ let ``ValidationFailure should convertExpectationInfo into a ValidationFailure``
                         Expected = $"%A{expected}"
                         Actual = $"%A{result}" 
                     } |> ExpectationVerificationFailure,
+                    Location.Get ()
+                )
+                |> TestExpectationFailure
+                |> TestFailure
+    )
+    
+let ``ValidationFailure should take an expected and an actual and return a ValidationFailure`` =
+    feature.Test (
+        fun builder _ ->
+            let result = builder.ValidationFailure ("45", false, "Z:/Test/blah.fs", 99)
+            let expected = TestFailure (TestExpectationFailure ((ExpectationVerificationFailure { Expected = "\"45\""; Actual = "false" }), { FilePath = "Z:\\Test"; FileName = "blah.fs"; LineNumber = 99 }))
+            
+            if result = expected then
+                TestSuccess
+            else
+                (
                     {
-                        FilePath = ".\\Tests"
-                        FileName = "TestResultFailureBuilder.fs"
-                        LineNumber = 16 
-                    }
+                        Expected = $"%A{expected}"
+                        Actual = $"%A{result}" 
+                    } |> ExpectationVerificationFailure,
+                    Location.Get ()
+                )
+                |> TestExpectationFailure
+                |> TestFailure
+    )
+
+let ``GeneralTestExpectationFailure should convert a message into a failure`` =
+    feature.Test (
+        fun builder _ ->
+            let result = builder.GeneralTestExpectationFailure ("A message as to why this fails", "G:\\Test\\Place\\file.fs", 87)
+            let expected = TestFailure (TestExpectationFailure ((ExpectationOtherFailure "A message as to why this fails"), { FilePath = "G:\\Test\\Place"; FileName = "file.fs"; LineNumber = 87 }))
+            
+            if result = expected then
+                TestSuccess
+            else
+                (
+                    {
+                        Expected = $"%A{expected}"
+                        Actual = $"%A{result}" 
+                    } |> ExpectationVerificationFailure,
+                    Location.Get ()
+                )
+                |> TestExpectationFailure
+                |> TestFailure
+    )
+    
+let ``IgnoreFailure should take a message and return a failure`` =
+    feature.Test (
+        fun builder _ ->
+            let result = builder.IgnoreFailure ("I ignored this", "F:\\A\\Dir\\aFile.fs", 29)
+            let expected = TestFailure (TestIgnored (Some "I ignored this", { FilePath = "F:\\A\\Dir"; FileName = "aFile.fs"; LineNumber = 29 }))
+            
+            if result = expected then
+                TestSuccess
+            else
+                (
+                    {
+                        Expected = $"%A{expected}"
+                        Actual = $"%A{result}" 
+                    } |> ExpectationVerificationFailure,
+                    Location.Get ()
+                )
+                |> TestExpectationFailure
+                |> TestFailure
+    )
+    
+let ``IgnoreFailure should take Some (message) and return a failure`` =
+    feature.Test (
+        fun builder _ ->
+            let result = builder.IgnoreFailure (Some "I ignored this also", "Q:\\myFile.fsx", 42)
+            let expected =TestFailure (TestIgnored (Some "I ignored this also", { FilePath = "Q:\\"; FileName = "myFile.fsx"; LineNumber = 42 }))
+            
+            if result = expected then
+                TestSuccess
+            else
+                (
+                    {
+                        Expected = $"%A{expected}"
+                        Actual = $"%A{result}" 
+                    } |> ExpectationVerificationFailure,
+                    Location.Get ()
+                )
+                |> TestExpectationFailure
+                |> TestFailure
+    )
+    
+let ``IgnoreFailure should take None and return a failure`` =
+    feature.Test (
+        fun builder _ ->
+            let result = builder.IgnoreFailure (None, "Y:\\Z\\a.xls", 1)
+            let expected = TestFailure (TestIgnored (None, { FilePath = "Y:\\Z"; FileName = "a.xls"; LineNumber = 1 }))
+            
+            if expected = result then
+                TestSuccess
+            else
+                (
+                    {
+                        Expected = $"%A{expected}"
+                        Actual = $"%A{result}" 
+                    } |> ExpectationVerificationFailure,
+                    Location.Get ()
+                )
+                |> TestExpectationFailure
+                |> TestFailure
+    )
+    
+let ``IgnoreFailure should return a failure`` =
+    feature.Test (
+        fun builder _ ->
+            let result = builder.IgnoreFailure ("E:\\t.ts", 90)
+            let expected = TestFailure (TestIgnored (None, { FilePath = "E:\\"; FileName = "t.ts"; LineNumber = 90 }))
+            
+            if result = expected then
+                TestSuccess
+            else
+                (
+                    {
+                        Expected = $"%A{expected}"
+                        Actual = $"%A{result}" 
+                    } |> ExpectationVerificationFailure,
+                    Location.Get ()
+                )
+                |> TestExpectationFailure
+                |> TestFailure
+    )
+
+let ``ExceptionFailure should convert an exception into a failure`` =
+    feature.Test (
+        fun builder _ ->
+            let ex = System.NotImplementedException ("Not Ready")
+            let result = builder.ExceptionFailure ex
+            let expected = TestFailure (TestExceptionFailure ex)
+            
+            if result = expected then
+                TestSuccess
+            else
+                (
+                    {
+                        Expected = $"%A{expected}"
+                        Actual = $"%A{result}" 
+                    } |> ExpectationVerificationFailure,
+                    Location.Get ()
                 )
                 |> TestExpectationFailure
                 |> TestFailure
---
+++ Tests/TypeHelpers.fs
@@ -0,0 +1,19 @@
+﻿[<AutoOpen>]
+module Archer.Fletching.Tests.TypeHelpers
+
+open Archer
+open System.IO
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+type Location =
+    static member Get([<CallerMemberName; Optional; DefaultParameterValue("")>] testName: string, [<CallerFilePath; Optional; DefaultParameterValue("")>] fileFullName: string, [<CallerLineNumber; Optional; DefaultParameterValue(-1)>]lineNumber: int) =
+        let fileInfo = FileInfo fileFullName
+        let filePath = fileInfo.Directory.FullName
+        let fileName = fileInfo.Name
+        
+        {
+            FilePath = filePath
+            FileName = fileName
+            LineNumber = lineNumber 
+        }

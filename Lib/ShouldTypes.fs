207179826c2a46605edfdfa1471f990a3268216b
jason-kerney <jason@teamkerney.com>
2023-04-22 08:39:25 -0700

    feat: Should.BeEqualTo returns failure if not equal

--- Lib/ShouldTypes.fs
+++ Lib/ShouldTypes.fs
@@ -3,10 +3,16 @@ module Archer.ShouldTypes
 
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
+open Archer.Fletching.Types.Internal
+
+let private failureBuilder = TestResultFailureBuilder id
 
 type Should =
     //Object Checks
     static member BeEqualTo<'a> (expected: 'a, [<CallerFilePath; Optional; DefaultParameterValue("")>] fullPath: string, [<CallerLineNumber; Optional; DefaultParameterValue(-1)>]lineNumber: int) =
         let check (actual: 'a) =
-            TestSuccess
+            if System.Object.ReferenceEquals (expected, actual) then
+                TestSuccess
+            else
+                failureBuilder.ValidationFailure (expected, actual, fullPath, lineNumber)
         check
--- Tests/Should Object Methods.fs
+++ Tests/Should Object Methods.fs
@@ -20,5 +20,20 @@ let ``BeEqualTo should return success if both items are the same object`` =
             thing
             |> Should.BeEqualTo thing
     )
+    
+let ``BeEqualTo should return a validation failure if they are different`` =
+    feature.Test (
+        fun _ ->
+            let thing1 = obj ()
+            let thing2 = obj ()
+            
+            let result = thing1 |> Should.BeEqualTo (thing2, "W:\\thingTest.tst", -24)
+            let expected = TestFailure (TestExpectationFailure ((ExpectationVerificationFailure { Expected = $"%A{thing1}"; Actual = $"%A{thing2}" }), { FilePath = "W:\\"; FileName = "thingTest.tst"; LineNumber = -24 }))
+            
+            if result = expected then
+                TestSuccess
+            else
+                failureBuilder.ValidationFailure (expected, result)
+    )
 
 let ``Test Cases`` = feature.GetTests ()

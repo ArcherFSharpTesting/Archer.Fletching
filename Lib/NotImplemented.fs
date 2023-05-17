2914fb8efdedabd2528ecd42d479e3cb0d9ed1b4
jason-kerney <jason@teamkerney.com>
2023-05-17 14:36:54 -0700

    feat: Not.Implemented

---
+++ Lib/NotImplemented.fs
@@ -0,0 +1,10 @@
+﻿namespace Archer
+
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+open Archer.Fletching.Types.Internal
+
+type Not =
+    static member Implemented ([<CallerFilePath; Optional; DefaultParameterValue("")>] fullPath: string, [<CallerLineNumber; Optional; DefaultParameterValue(-1)>]lineNumber: int) =
+        let failureBuilder = TestResultFailureBuilder id
+        failureBuilder.IgnoreFailure ("Not Yet Implemented", fullPath, lineNumber)

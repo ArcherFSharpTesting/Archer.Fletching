7deaa9b00b5968e898d053eb8fff4ff8778b4f9c
Jason Kerney <Jason@TeamKerney.com>
2025-08-28 07:55:03 -0700

    docs: doc comments

--- Lib/NotImplemented.fs
+++ Lib/NotImplemented.fs
@@ -4,7 +4,15 @@ open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 open Archer.Fletching.Types.Internal
 
+/// <summary>
+/// Provides a method to indicate that a test or feature is not yet implemented.
+/// </summary>
 type Not =
+    /// <summary>
+    /// Marks a test or feature as not yet implemented. This does not throw an exception, but returns a failure result for reporting.
+    /// </summary>
+    /// <param name="fullPath">The file path of the calling code. Automatically provided by the compiler.</param>
+    /// <param name="lineNumber">The line number in the source file. Automatically provided by the compiler.</param>
     static member Implemented ([<CallerFilePath; Optional; DefaultParameterValue("")>] fullPath: string, [<CallerLineNumber; Optional; DefaultParameterValue(-1)>] lineNumber: int) =
         let failureBuilder = TestResultFailureBuilder id
         failureBuilder.IgnoreFailure ("Not Yet Implemented", fullPath, lineNumber)
